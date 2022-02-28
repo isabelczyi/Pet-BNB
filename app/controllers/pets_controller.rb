5df67a16374d3260e19039a3b07df4d75152fd47
Isabel Cheung <isabelczyi@gmail.com>
2022-02-28 15:37:17 +1000

    added new create show index for pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,8 +6,26 @@ class PetsController < ApplicationController
   end
 
   def new
+    @pet = Pet.new
   end
 
   def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :category, :location, :price, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :pets, only: [:index, :new, :create]
+  resources :pets, only: [:index, :new, :create, :show]
 end
