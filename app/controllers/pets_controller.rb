917d63271791a08350e0e4a289ba6d703da9ad57
Isabel Cheung <isabelczyi@gmail.com>
2022-03-02 14:58:47 +1000

    last minute changes done

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,12 +3,12 @@ class PetsController < ApplicationController
 
   def index
     @pets = policy_scope(Pet).order(created_at: :desc)
-    @markers = @pets.geocoded.map do |pet|
-      {
-        lat: pet.latitude,
-        lng: pet.longitude
-      }
-    end
+    # @markers = @pets.geocoded.map do |pet|
+    #   {
+    #     lat: pet.latitude,
+    #     lng: pet.longitude
+    #   }
+    # end
   end
 
   def new
