87bd3efc7ef7fda73168ffe4b62e70c1f4d4c609
Isabel Cheung <isabelczyi@gmail.com>
2022-03-03 16:25:30 +1000

    heroku time

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,22 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :pet
+
+  validate :date_cannot_be_in_the_past
+  validate :start_must_be_before_end_time
+
+  def date_cannot_be_in_the_past
+    if check_in.present? && check_in < Date.today
+      errors.add(:check_in, "can't be in the past")
+    end
+
+    if check_out.present? && check_out < Date.today
+      errors.add(:check_out, "can't be in the past")
+    end
+  end
+
+  def start_must_be_before_end_time
+    errors.add(:check_in, "must be before end time") unless
+          check_in < check_out
+  end
 end
