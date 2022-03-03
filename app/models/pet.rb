97c51c5567814cb7923b2a5d719a587245edbff8
Isabel Cheung <isabelczyi@gmail.com>
2022-03-04 00:20:06 +1000

    fixed bugs

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -6,4 +6,8 @@ class Pet < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode, if: :will_save_change_to_address?
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :category, presence: true
 end
